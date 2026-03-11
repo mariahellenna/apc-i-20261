3993267ecc2343e7b4e11696cc85436888540e64
unknown <2612130045@ACADEMICO.IESB.BR>
2026-03-11 10:57:40 -0300

    adiciona codigos da aula02

--- aulas/aula01/ola_mundo.c
+++ aulas/aula01/ola_mundo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Banana frita");
+    printf("Olá mundo");
     return 0;
 }
---
+++ aulas/aula02/saida.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // inclusao de bibliotecas da linguagem
+
+
+int main () {
+    
+
+   printf("um texto qualquer\n");
+   printf("Programar em C eh top!\n");
+   printf("%i\n", 10);
+
+    return 0;
+}
